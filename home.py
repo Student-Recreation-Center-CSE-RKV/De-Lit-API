5d64e257f04dc0f1a0c02d6216da3ccf60d7d6f3
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-09-22 15:49:12 +0530

    Test-2

--- home.py
+++ home.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
+import pymongo
+import json
+
 app = APIRouter()
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
 mydb = client['phani']
-connection = client['home_details']
+connection = mydb.Phani
 
-#endpoint
-@app.get('get_contact')
+#end
+@app.get('/get_contact')
 async def get_contact():
-    return None
+    data = list(connection.find())
+    for doc in data:
+        doc["_id"] = str(doc["_id"])
+    return data
+
 # "https://raw.githubusercontent.com/venkataPhanindraVutla/Demo-Names/blob/main/images/R200026.jpg"
