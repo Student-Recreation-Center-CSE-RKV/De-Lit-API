acbb8833057c75dbfd46aea7b3e788a55edbe252
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-09-24 16:10:22 +0530

    Home endpoints completed

---
+++ magazine.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from utils import client
+import json
+
+app = APIRouter()
+mydb = client['Delit-test']
+connection = mydb.home
+
+@app.get("/get-info")
+async def get_info():
+    data = list(connection.find())
+    for doc in data:
+        doc["_id"] = str(doc["_id"])
+    return data
