b5948242b1620a32fc64ca87fb6572cea830cf2e
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-09-25 22:13:35 +0530

    test_commit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-import home
+import home,magazine
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
-import magazine
 
 app = FastAPI()
 url = "mongodb+srv://phanivutla2004:phaniphani@cluster0.gddku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
