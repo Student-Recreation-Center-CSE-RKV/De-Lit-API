8d6b50fca92cc035f52b68df85b0790f5e645358
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-10-11 11:42:30 +0530

    chore blog.md,home.md,add banner endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-import home,publications,blog
+import home,publications,blog,banner
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
@@ -8,3 +8,4 @@ app = FastAPI()
 app.include_router(home.app, prefix="/home")
 app.include_router(publications.app, prefix="/publications")
 app.include_router(blog.app,prefix = "/blog")
+app.include_router(banner.app,prefix = "/banner")
